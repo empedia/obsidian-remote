a48edd39049dfd4d5eaa836f8a32aed6be068e2c
empedia <empedia@gmail.com>
2023-07-23 13:15:50 +0000

    wrapped openbox session with dbus

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-/startpulse.sh &
-/usr/bin/openbox-session > /dev/null 2>&1
+#/startpulse.sh &
+dbus-launch --exit-with-session /usr/bin/openbox-session > /dev/null 2>&1
+#/usr/bin/openbox-session > /dev/null 2>&1
